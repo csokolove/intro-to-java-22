94c2e73c0ea957bbadd25dcd33f977b1e3f122ff
Charlie <94080789+csokolove@users.noreply.github.com>
2022-07-26 00:11:12 -0400

    day 2

---
+++ Conditionals.java
@@ -0,0 +1,13 @@
+public class Conditionals {
+    static boolean x = true;
+    static boolean y = false;
+    public static void main(String[] args) {
+        if(x) {
+            // code executed if x is true
+        } else if (y) {
+            // code executed if x is false and y is true
+        } else {
+            // code execute if both x and y are false
+        }
+    }
+}
